2dad02a147df59b02fe5f3d0efc733292c8dce4b
unknown <berimecdanil963@gmail.com>
2023-02-24 17:15:47 +0300

    change

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let textinput = document.querySelector('.input_text')
+let inp = document.querySelector('.input_text')
 
 let blockname = document.querySelector('.message')
 
@@ -6,33 +6,79 @@ let btnPush = document.querySelector('.buttonRun')
 
 let btnClear = document.querySelector('.buttonClear')
 
-let newtextinput = "";
+let gender = document.querySelector('.genderselect')
+
+let name = "";
 
 let html = []
 
+let genimg = [
+   { image: "./img/01.png" },
+   { image: "./img/02.png" },
+   { image: "./img/03.png" },
+]
+
+
 btnPush.addEventListener('click', () => {
-   newtextinput = textinput.value
-   init()
-   render()
+   let name = inp.value
+   if (gender.value === "male") {
+      html.push({ name: inp.value, image: genimg[0].image });
+      render()
+      return
+   }
+   else if (gender.value === "female") {
+      html.push({ name: inp.value, image: genimg[1].image });
+      render()
+      return
+   }
+   else if (gender.value === "other") {
+      html.push({ name: inp.value, image: genimg[1].image });
+      render()
+      return
+   }
+
 })
 
 btnClear.addEventListener('click', () => {
-   blockname.innerHTML = '';
    html.splice(0)
+   blockname.innerHTML = '';
 })
 
-const init = function () {
-   const newhtml = html.push(newtextinput)
-   console.log(html)
+const render = () => {
+   const newhtml = html.map((item) => {
+      const structhtml = `<li class="li">${item.name}<img class="img" src="${item.image}"></li>`
+      return structhtml;
+   })
+
+   blockname.innerHTML = html.join('');
 }
 
-const render = function () {
-   const renderFun = html.map((item) => {
-      let newname = `<li>${item}</li>`
-      return newname
-   })
-   blockname.innerHTML = renderFun.join('');
-}
+
+
+// btnPush.addEventListener('click', () => {
+//    newtextinput = textinput.value
+//    init()
+//    render()
+//    choisegender()
+// })
+
+// btnClear.addEventListener('click', () => {
+//    blockname.innerHTML = '';
+//    html.splice(0)
+// })
+
+// const init = function () {
+//    const newhtml = html.push(newtextinput)
+//    console.log(html)
+// }
+
+// const render = function () {
+//    const renderFun = html.map((item) => {
+//       let newname = `<li>${item.image}<img class="img" src="${item.image}"</li> `
+//       return newname
+//    })
+//    blockname.innerHTML = renderFun.join('');
+// }
 
 
 
