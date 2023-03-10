d0b7806406c8897278aeb498d34bea41f3e846fc
unknown <berimecdanil963@gmail.com>
2023-03-10 20:22:07 +0300

    hw

--- js/script.js
+++ js/script.js
@@ -1,95 +1,16 @@
-let inp = document.querySelector('.input_text')
+let container = document.querySelector('.container')
 
-let blockname = document.querySelector('.message')
-
-let btnPush = document.querySelector('.buttonRun')
-
-let btnClear = document.querySelector('.buttonClear')
-
-let gender = document.querySelector('.genderselect')
-
-let name = "";
-
-let html = []
-
-let genimg = [
-   { image: "./img/01.png" },
-   { image: "./img/02.png" },
-   { image: "./img/03.png" },
-]
+let block = document.querySelector('.block')
 
 
-btnPush.addEventListener('click', () => {
-   let name = (inp.value);
-   if (inp.value === '') {
-      return
-   }
-   else if (gender.value === 'male') {
-      html.push({ name: inp.value, image: genimg[0].image });
-      render()
-      return
-   }
-   else if (gender.value === 'female') {
-      html.push({ name: inp.value, image: genimg[1].image });
-      render()
-      return
-   }
-   else if (gender.value === 'other') {
-      html.push({ name: inp.value, image: genimg[2].image });
-      render()
-      return
-   }
-
+block.addEventListener('click', () => {
+   x = Math.floor(Math.random() * 1000) + "px"
+   y = Math.floor(Math.random() * 700) + "px"
+   renderEl();
 })
-const render = () => {
-   const newhtml = html.map((item) => {
-      structhtml = `<li class="li">${item.name}<img class="img" src="${item.image}"></li>`
-      return structhtml;
-   })
 
-   blockname.innerHTML = newhtml.join('');
+const renderEl = () => {
+   block.style.left = x;
+   block.style.top = y;
+   return
 }
-
-btnClear.addEventListener('click', () => {
-   html.splice(0)
-   blockname.innerHTML = '';
-})
-
-
-
-
-
-// btnPush.addEventListener('click', () => {
-//    newtextinput = textinput.value
-//    init()
-//    render()
-//    choisegender()
-// })
-
-// btnClear.addEventListener('click', () => {
-//    blockname.innerHTML = '';
-//    html.splice(0)
-// })
-
-// const init = function () {
-//    const newhtml = html.push(newtextinput)
-//    console.log(html)
-// }
-
-// const render = function () {
-//    const renderFun = html.map((item) => {
-//       let newname = `<li>${item.image}<img class="img" src="${item.image}"</li> `
-//       return newname
-//    })
-//    blockname.innerHTML = renderFun.join('');
-// }
-
-
-
-
-
-
-
-
-
-
